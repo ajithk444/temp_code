01b570e396f086ef4e4b26cdfbb840902bcc2854
Ajith <ajithk444@gmail.com>
2018-09-26 15:15:17 +0530

    Update codeChanges.cs

--- codeChanges.cs
+++ codeChanges.cs
@@ -1,3 +1,34 @@
-    StartDate =  string.Format("{0:yyyy-MM-dd hh:mm:ss}", task.StartDate),
-                    ActualStartDate = string.Format("{0:yyyy-MM-dd hh:mm:ss}", task.ActualStartDate),
-                    ActualEndDate = task.IsComplete ? string.Format("{0:yyyy-MM-dd hh:mm:ss}", task.ActualEndDate) : string.Empty,
+ private bool CheckIfCyclicDirectedGraph(int childTaskID, int parentTaskID)
+        {
+            bool isCyclic = false;
+            Dictionary<int, HashSet<int>> adjList = new Dictionary<int, HashSet<int>>();
+           
+
+            var tasks = taskHelper.TaskRepository.Get_TaskListByProjectID(CurrentUser.CurrentUSER.CurrentProjectID, true).ToList();
+
+            //get all edges
+            AdjacencyList adjacencyList = new AdjacencyList(Convert.ToInt32(tasks.Count * tasks.Count),true);
+            foreach (var item in tasks)
+            {
+                //check in left (child) tree
+                var parentTasks = taskHelper.TaskRepository.GetParentTasksByChildId(item.ID);
+                foreach (var parentTask in parentTasks)
+                {
+                    adjacencyList.AddEdge(parentTask.ID, item.ID);
+                }
+                //check in  right (parent) tree
+                var childTasks = taskHelper.TaskRepository.GetChildTasksByParentId(item.ID);
+                foreach (var childItem in childTasks)
+                {
+                    adjacencyList.AddEdge(item.ID,childItem.ID);
+                }
+            }
+
+            //add the new link to the list to check if it becomes a cyclic tree
+            adjacencyList.AddEdge(parentTaskID,childTaskID);
+
+            CycleDirectedGraph cycleDirectedGraph = new CycleDirectedGraph(adjacencyList);
+            //cycleDirectedGraph.DetectCycle();
+            isCyclic = cycleDirectedGraph.DetectCycleWithColor();
+            return isCyclic;
+        }
