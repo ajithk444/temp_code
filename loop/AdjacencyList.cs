6f4afbacefda3905b98e05d6ffe1c09d8eaec8e2
Ajith <6139652+ajithk444@users.noreply.github.com>
2018-10-14 08:10:18 +0530

    Add files via upload

---
+++ loop/AdjacencyList.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test
+{
+    public class AdjacencyList
+    {
+        public int V { get; set; }
+        public int[] TasksArray { get; set; }
+        public LinkedList<int>[] AdjListArray { get; set; }
+        public bool[] Visited { get; set; }
+        public bool IsDirected { get; set; }
+
+        public AdjacencyList(int v, int[] items, bool isDirected = false)
+        {
+            V = v;
+            AdjListArray = new LinkedList<int>[V];
+
+            for (var i = 0; i < AdjListArray.Length; i++)
+            {
+                AdjListArray[i] = new LinkedList<int>();
+            }
+
+            Visited = new bool[V];
+            IsDirected = isDirected;
+            TasksArray = items;
+        }
+
+        public void Reset()
+        {
+            for (var i = 0; i < Visited.Length; i++)
+            {
+                Visited[i] = false;
+            }
+        }
+
+        public void AddEdge(int src, int des)
+        {
+            int getSourceIndex = GetIndex(src);
+            int getDestinationIndex = GetIndex(des);
+            AdjListArray[getSourceIndex].AddLast(getDestinationIndex);
+            if (!IsDirected)
+            {
+                AdjListArray[getDestinationIndex].AddLast(getSourceIndex);
+            }
+
+        }
+
+        private int GetIndex(int value)
+        {
+            int position = 0;
+            foreach (var item in TasksArray)
+            {
+                if (item == value)
+                {
+                    return position;
+                }
+                position++;
+            }
+            return -1;
+        }
+
+        public void DisplayGraphBFS()
+        {
+            bool[] visited = new bool[V];
+
+            Queue<int> queue = new Queue<int>();
+            Console.Write("Please enter the start point : ");
+            int start = int.Parse(Console.ReadLine());
+            queue.Enqueue(start);
+            visited[start] = true;
+
+            while (queue.Count > 0)
+            {
+                int next = queue.Dequeue();
+                Console.Write(next + " ");
+                foreach (int i in AdjListArray[next])
+                {
+                    if (visited[i] != true)
+                    {
+                        queue.Enqueue(i);
+                        visited[i] = true;
+                    }
+                }
+            }
+        }
+
+        public void DisplayGraphDFS(int start)
+        {
+            Console.Write(start + " ");
+            Visited[start] = true;
+
+            foreach (int i in AdjListArray[start])
+            {
+                if (Visited[i] != true)
+                {
+                    DisplayGraphDFS(i);
+                    Visited[i] = true;
+                }
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < V; i++)
+            {
+                foreach (int item in AdjListArray[i])
+                {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ loop/CycleDirectedGraph.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Test;
+
+namespace Test.Algorithm
+{
+    public class CycleDirectedGraph
+    {
+        public AdjacencyList AdjList { get; set; }
+
+        public CycleDirectedGraph(AdjacencyList adjList)
+        {
+            AdjList = adjList;
+        }
+
+        public void DetectCycle()
+        {
+            if (HasCycle())
+            {
+                Console.WriteLine("There is a cyle");
+            }
+            else
+            {
+                Console.WriteLine("No cycle");
+            }
+
+        }
+
+        public bool HasCycle()
+        {
+            int v = AdjList.V;
+            bool[] inStack = new bool[v];
+            for (int i = 0; i < v; i++)
+            {
+                if (!AdjList.Visited[i] && DFSUtil(i, inStack))
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+        private bool DFSUtil(int i, bool[] inStack)
+        {
+            AdjList.Visited[i] = true;
+            inStack[i] = true;
+
+            foreach (int child in AdjList.AdjListArray[i])
+            {
+                if (inStack[child])
+                {
+                    return true;
+                }
+
+                if (!AdjList.Visited[child] && DFSUtil(child, inStack))
+                {
+                    return true;
+                }
+            }
+
+            inStack[i] = false;
+            return false;
+        }
+
+        //WHITE : Vertex is not processed yet.Initially
+        //    all vertices are WHITE.
+
+        //    GRAY : Vertex is being processed(DFS for this
+        //vertex has started, but not finished which means
+        //    that all descendants (ind DFS tree) of this vertex
+        //    are not processed yet (or this vertex is in function
+        //    call stack)
+
+        //BLACK : Vertex and all its descendants are processed.
+
+        public enum Color
+        {
+            White,
+            Gray,
+            Black
+        }
+
+        public bool DetectCycleWithColor()
+        {
+            if (HasCycleWithColor())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public bool HasCycleWithColor()
+        {
+            int v = AdjList.V;
+            Color[] colors = new Color[v];
+            for (int i = 0; i < colors.Length; i++)
+            {
+                colors[i] = Color.White;
+            }
+
+            for (int i = 0; i < v; i++)
+            {
+                if (colors[i] == Color.White && DFSUtilWithColor(i, colors))
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+        private bool DFSUtilWithColor(int i, Color[] colors)
+        {
+            colors[i] = Color.Gray;
+
+            foreach (int child in AdjList.AdjListArray[i])
+            {
+                if (colors[child] == Color.Gray)
+                {
+                    return true;
+                }
+
+                if (colors[child] == Color.White && DFSUtilWithColor(child, colors))
+                {
+                    return true;
+                }
+            }
+
+            colors[i] = Color.Black;
+            return false;
+        }
+    }
+}
